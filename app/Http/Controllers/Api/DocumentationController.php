dc5030c2e1a88242af16d863b335fb0de8765ffe
premiumajans <mmcvakant@gmail.com>
2023-04-17 12:13:23 +0400

    auth 2

--- app/Http/Controllers/Api/DocumentationController.php
+++ app/Http/Controllers/Api/DocumentationController.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 /**
- * Create Vakant
- * @OA\Get  (
- *     path="/api/",
- *     tags={"Vakant"},
+ *
+ * @OA\POST  (
+ *     path="/api/login",
+ *     tags={"Auth"},
  *     @OA\RequestBody(
  *         @OA\MediaType(
  *             mediaType="application/json",
@@ -17,17 +17,17 @@ use Illuminate\Http\Request;
  *                 @OA\Property(
  *                      type="object",
  *                      @OA\Property(
- *                          property="title",
+ *                          property="email",
  *                          type="string"
  *                      ),
  *                      @OA\Property(
- *                          property="content",
+ *                          property="password",
  *                          type="string"
  *                      )
  *                 ),
  *                 example={
- *                     "title":"example title",
- *                     "content":"example content"
+ *                     "email":"example@gmail.com",
+ *                     "password":"Password"
  *                }
  *             )
  *         )
@@ -51,14 +51,83 @@ use Illuminate\Http\Request;
  *          )
  *      )
  * )
+ *
+ *
+ * Logout
+ * @OA\POST  (
+ *     path="/api/logout",
+ *     tags={"Auth"},
+ *      @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      type="object",
+ *                      @OA\Property(
+ *                          property="token",
+ *                          type="string"
+ *                      ),
+ *                 ),
+ *                 example={
+ *                     "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdmFrYW50LnRlc3QvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2ODE3MTIyMjQsImV4cCI6MTY4MTcxNTgyNCwibmJmIjoxNjgxNzEyMjI0LCJqdGkiOiJwWjExckJxTUl1bnNBak5jIiwic3ViIjoiMSIsInBydiI6ImRmODgzZGI5N2JkMDVlZjhmZjg1MDgyZDY4NmM0NWU4MzJlNTkzYTkifQ.H4gd3Iez5hJKCV_GExHFFFPo08ga8WtqFhm0mHaAL-s",
+ *                }
+ *             )
+ *         )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="success",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="status", type="number", example="success"),
+ *              @OA\Property(property="message", type="string", example="successfully-logged-out"),
+ *          )
+ *      ),
+ * )
+ *
+ *
+ * Refresh
+ * @OA\POST  (
+ *     path="/api/refresh",
+ *     tags={"Auth"},
+ *      @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                      type="object",
+ *                      @OA\Property(
+ *                          property="token",
+ *                          type="string"
+ *                      ),
+ *                 ),
+ *                 example={
+ *                     "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdmFrYW50LnRlc3QvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2ODE3MTIyMjQsImV4cCI6MTY4MTcxNTgyNCwibmJmIjoxNjgxNzEyMjI0LCJqdGkiOiJwWjExckJxTUl1bnNBak5jIiwic3ViIjoiMSIsInBydiI6ImRmODgzZGI5N2JkMDVlZjhmZjg1MDgyZDY4NmM0NWU4MzJlNTkzYTkifQ.H4gd3Iez5hJKCV_GExHFFFPo08ga8WtqFhm0mHaAL-s",
+ *                }
+ *             )
+ *         )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="success",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="status", type="number", example="success"),
+ *              @OA\Property(
+ *              property="authorisation",
+ *               type="array",
+ *               description="The survey ID",
+ *               @OA\Items(
+ *                  property="firstName",
+ *                  type="string",
+ *                  example=""
+ *               ),
+ *           ),
+ *          )
+ *      ),
+ * )
+ *
  */
+
+
 class DocumentationController extends Controller
 {
-    public function index(Request $request)
-    {
-        return response()->json([
-            'name' => $request->input('name'),
-            'message' => 'Hello World!'
-        ]);
-    }
 }
